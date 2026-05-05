788331d16f3a14f0d7be444a092f1e54e8c492b8
PRASHANT SAINI <Prashant.saini.3150@gmail.com>
2026-05-05 21:22:30 +0530

    feat: initialize React project with Vite configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
